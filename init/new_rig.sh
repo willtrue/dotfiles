2602af045b67cc7b93c35370a508d69814542a44
willtrue <wt@willtrue.com>
2014-03-15 14:05:11 -0700

    fixes setting the NetBIOSName

--- init/new_rig.sh
+++ init/new_rig.sh
@@ -4,7 +4,7 @@
 sudo scutil --set ComputerName "wt_"
 sudo scutil --set HostName "wt_"
 sudo scutil --set LocalHostName "wt_"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "wt_"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName "wt_"
 
 # Launch Sublime Text 2 from command line as 'subl' based on: https://gist.github.com/artero/1236170
 ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
